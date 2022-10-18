957358bfcf1d7ce02c8109f51efd36f13b741fba
SLeBron <sophieclebron@gmail.com>
2022-10-17 21:23:10 -0600

    refactored connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoURI: 'mongodb+srv://slebron:password7272@cluster0.0g8zrjg.mongodb.net/?retryWrites=true&w=majority'
+
+const uri = process.env.MONGODB_URI;
+// process.env.config_variable_name
+
+mongoose
+.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.log('Error:- ' + err));
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
 
 module.exports = mongoose.connection;
